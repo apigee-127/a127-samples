f2eb80708fc500663411ed10e0e101db2e5e53bf
Jeff West <jwest@apigee.com>
2014-10-29 13:35:55 -0700

    draft 1

--- weather-a127-resources/api/controllers/hello_world.js
+++ weather-a127-resources/api/controllers/hello_world.js
@@ -3,11 +3,27 @@
 var util = require('util');
 
 module.exports = {
-  hello: hello
+  get: hello
 };
 
 function hello(req, res) {
   var name = req.swagger.params.name.value;
-  var hello = name ? util.format('Hello, %s', name) : 'Hello, stranger!';
-  res.json(hello);
+
+  if (!name) {
+    name = 'stranger';
+  }
+
+  var cache = req.a127.resource('mycache');
+
+  cache.get(name, function (err, data) {
+
+    if (data) {
+      res.json(util.format('Hello, %s - CACHEMASTER!  Cache Value: %s', name, data));
+    }
+    else {
+      cache.set(name, 'foobar', function (err, data) {
+      });
+      res.json(util.format('Hello, %s!', name));
+    }
+  });
 }
