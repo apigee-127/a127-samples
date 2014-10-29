b5368e552f51dc0fd0d5e321635a985a5e286407
Jeff West <jwest@apigee.com>
2014-10-29 14:03:49 -0700

    cleaned up

--- weather-a127-resources/api/controllers/weather.js
+++ weather-a127-resources/api/controllers/weather.js
@@ -4,7 +4,7 @@ var util = require('util');
 var request = require('request');
 
 module.exports = {
-  getWeatherByCity: getWeatherByCity
+  get: getWeatherByCity
 };
 
 function getWeatherByCity(req, res) {
@@ -15,33 +15,41 @@ function getWeatherByCity(req, res) {
 
   console.log('Executing request: ' + url);
 
-  cache.get(city, function (err, data) {
-    if (err) {
-      res.status(500).send(err)
-    }
+  if (cache) {
+    cache.get(city, function (err, data) {
+      if (err) {
+        res.status(500).send(err)
+      }
 
-    else if (data) {
-      res.json(data);
-    }
+      else if (data) {
+        console.log('Cache hit!');
+        res.json(data);
+      }
 
-    else {
-      request.get(url, function (err, response, body) {
-        if (err) {
-          res.status(500).send(err)
-        }
+      else {
+        request.get(url, function (err, response, body) {
+          if (err) {
+            res.status(500).send(err)
+          }
 
-        else {
-          cache.set(city, body, function (err, data) {
-            if (err) {
-              res.status(500).send(err)
-            }
+          else {
+            console.log('Cache miss!');
+            cache.set(city, body, function (err, data) {
+              if (err) {
+                res.status(500).send(err)
+              }
 
-            else {
-              res.send(data);
-            }
-          });
-        }
-      });
-    }
-  });
+              else {
+                res.send(data);
+              }
+            });
+          }
+        });
+      }
+    });
+  }
+  else {
+    console.log('Cache not found!');
+    request.get(url).pipe(res);
+  }
 }
