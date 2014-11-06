29534eafd3c1f8d844e125e41bc133929ecc4f1f
Jeff West <jwest@apigee.com>
2014-11-06 14:37:54 -0800

    changed to use curl from wget

--- cache-example/redis.sh
+++ cache-example/redis.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-wget http://download.redis.io/releases/redis-2.8.17.tar.gz
+curl -O http://download.redis.io/releases/redis-2.8.17.tar.gz
 tar xzf redis-2.8.17.tar.gz
 cd redis-2.8.17
 make
