ea6014c78759af8cc9c18b41bc905f3956bf4997
Jeff West <jwest@apigee.com>
2014-12-15 18:06:51 -0800

    WIP

--- a127-quota-programmatic/api/controllers/quota.js
+++ a127-quota-programmatic/api/controllers/quota.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var util = require('util');
-
-module.exports.clearCache = function (req, res) {
-  var cache = req.a127.resource('mycache');
+module.exports.delete = function (req, res) {
+  var cache = req.a127.resource('myquota');
 
   if (cache) {
     cache.clear(function (err) {
@@ -11,18 +9,18 @@ module.exports.clearCache = function (req, res) {
         res.status(500).send(err);
       }
       else {
-        res.status(200).send('cache successfully cleared');
+        res.status(200).send('quota successfully reset');
       }
     })
   }
   else {
-    res.status(500).send('Cache not found');
+    res.status(500).send('service not found');
   }
 };
 
 
 module.exports.get = function (req, res) {
-  var cache = req.a127.resource('mycache');
+  var cache = req.a127.resource('myquota');
 
   var key = req.swagger.params.key.value;
 
@@ -43,37 +41,6 @@ module.exports.get = function (req, res) {
     });
   }
   else {
-    res.status(500).send('cache not found');
-  }
-};
-
-module.exports.delete = function (req, res) {
-  var cache = req.a127.resource('mycache');
-
-  var key = req.swagger.params.key.value;
-
-  if (cache) {
-    cache.get(key, function (err, data) {
-      if (err) {
-        res.status(500).send(err)
-      }
-      else if (data) {
-        cache.delete(key, function (err) {
-          if (err) {
-            res.status(500).send(err)
-          }
-          else {
-            res.status(200).send('Deleted value for key=' + key + '!');
-          }
-        });
-      }
-      else
-      {
-        res.status(404).send('Key=[' + key + '] not found!');
-      }
-    });
-  }
-  else {
-    res.status(500).send('error accessing cache');
+    res.status(500).send('service not found');
   }
 };
--- a127-quota-programmatic/api/controllers/weather.js
+++ a127-quota-programmatic/api/controllers/weather.js
@@ -2,49 +2,19 @@
 
 var request = require('request');
 
-module.exports.get = function(req, res) {
-  var cache = req.a127.resource('mycache');
+module.exports.get = function (req, res) {
   var city = req.swagger.params.city.value;
   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial";
 
-  if (cache) {
-    cache.get(city, function (err, data) {
-      if (err) {
-        res.status(500).send(err)
-      }
+  console.log('-+- Executing request: ' + url);
 
-      else if (data) {
-        console.log('Cache hit!');
-        res.json(data);
-      }
+  request.get(url, function (err, response, body) {
+    if (err) {
+      res.status(500).send(err)
+    }
 
-      else {
-        console.log('Cache miss!');
-
-
-        console.log('-+- Executing request: ' + url);
-
-        request.get(url, function (err, response, body) {
-          if (err) {
-            res.status(500).send(err)
-          }
-
-          else {
-            cache.set(city, body, function (err) {
-              if (err) {
-                res.status(500).send(err)
-              }
-              else {
-                res.send(body);
-              }
-            });
-          }
-        });
-      }
-    });
-  }
-  else {
-    console.log('Cache not found!');
-    request.get(url).pipe(res);
-  }
+    else {
+      res.send(body);
+    }
+  });
 };
