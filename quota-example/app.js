c774cfbffac96c817500fe687fabd0a1484637af
Scott Ganyo <scott@ganyo.com>
2014-12-16 14:01:36 -0800

    update all to use async init & 127.0.0.1 instead of localhost

--- quota-example/app.js
+++ quota-example/app.js
@@ -4,11 +4,10 @@ var a127 = require('a127-magic');
 var express = require('express');
 var app = express();
 
-// uncomment the following if you need to parse incoming form data
-//app.use(express.bodyParser());
+a127.init(function(config) {
+  app.use(a127.middleware(config));
 
-app.use(a127.middleware());
+  app.listen(process.env.PORT || 10010);
 
-app.listen(process.env.PORT || 10010);
-
-console.log('try this:\nhttp://localhost:10010/weather?city=Kinston,NC');
+  console.log('try this:\nhttp://127.0.0.1:10010/weather?city=Kinston,NC');
+});
