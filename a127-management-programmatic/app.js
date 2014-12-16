f2840ea0168e8be751ac23b4af95308426efde16
Scott Ganyo <scott@ganyo.com>
2014-12-16 13:48:23 -0800

    update all to use async init & 127.0.0.1 instead of localhost

--- a127-management-programmatic/app.js
+++ a127-management-programmatic/app.js
@@ -4,11 +4,9 @@ var a127 = require('a127-magic');
 var express = require('express');
 var app = express();
 
-// uncomment the following if you need to parse incoming form data
-//app.use(express.bodyParser());
+a127.init(function(config) {
+  app.use(a127.middleware(config));
+  app.listen(process.env.PORT || 10010);
 
-app.use(a127.middleware());
-
-app.listen(process.env.PORT || 10010);
-
-console.log('\nHit this URL and the cache will get an entry:\ncurl http://localhost:10010/weather/kinston,nc');
+  console.log('\nHit this URL and the cache will get an entry:\ncurl http://127.0.0.1:10010/weather/kinston,nc');
+});
--- a127-quota-programmatic/app.js
+++ a127-quota-programmatic/app.js
@@ -4,15 +4,14 @@ var a127 = require('a127-magic');
 var express = require('express');
 var app = express();
 
-// uncomment the following if you need to parse incoming form data
-//app.use(express.bodyParser());
+a127.init(function(config) {
+  app.use(a127.middleware(config));
 
-app.use(a127.middleware());
+  app.listen(process.env.PORT || 10010);
 
-app.listen(process.env.PORT || 10010);
+  console.log('\nHit this URL and the quota counter will go up:\ncurl http://127.0.0.1:10010/weather/kinston,nc');
 
-console.log('\nHit this URL and the quota counter will go up:\ncurl http://localhost:10010/weather/kinston,nc');
+  console.log('\nHit URL to increment the quota counter:\ncurl http://127.0.0.1:10010/cache/kinston,nc');
 
-console.log('\nHit URL to increment the quota counter:\ncurl http://localhost:10010/cache/kinston,nc');
-
-console.log('\nHit URL to reset the quota for a key:\ncurl -X DELETE http://localhost:10010/cache/kinston,nc');
+  console.log('\nHit URL to reset the quota for a key:\ncurl -X DELETE http://127.0.0.1:10010/cache/kinston,nc');
+});
