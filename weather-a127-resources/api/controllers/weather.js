c8a07898e054c4788fcd323f18484117f1233b81
Jeff West <jwest@apigee.com>
2014-12-15 16:04:45 -0800

    small updates

--- weather-a127-resources/api/controllers/weather.js
+++ weather-a127-resources/api/controllers/weather.js
@@ -3,17 +3,10 @@
 var util = require('util');
 var request = require('request');
 
-module.exports = {
-  get: getWeatherByCity
-};
-
-function getWeatherByCity(req, res) {
+module.exports.get = function(req, res) {
   var cache = req.a127.resource('mycache');
-
   var city = req.swagger.params.city.value;
   var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial";
-
-  console.log('Executing request: ' + url);
 
   if (cache) {
     cache.get(city, function (err, data) {
@@ -27,20 +20,23 @@ function getWeatherByCity(req, res) {
       }
 
       else {
+        console.log('Cache miss!');
+
+
+        console.log('Executing request: ' + url);
+
         request.get(url, function (err, response, body) {
           if (err) {
             res.status(500).send(err)
           }
 
           else {
-            console.log('Cache miss!');
             cache.set(city, body, function (err, data) {
               if (err) {
                 res.status(500).send(err)
               }
-
               else {
-                res.send(data);
+                res.send(body);
               }
             });
           }
@@ -52,4 +48,4 @@ function getWeatherByCity(req, res) {
     console.log('Cache not found!');
     request.get(url).pipe(res);
   }
-}
+};
