80669b966ffab92fd7aa7547d8d06b3f1c95ab1b
Mike Malloy <mike.malloy.2004@gmail.com>
2015-01-31 15:28:17 -0800

    Updated samples
    Added missing package dependencies and parameter entries

--- spikearrest-example/app.js
+++ spikearrest-example/app.js
@@ -7,8 +7,9 @@ var app = express();
 // uncomment the following if you need to parse incoming form data
 //app.use(express.bodyParser());
 
-app.use(a127.middleware());
+a127.init(function(config) {
+  app.use(a127.middleware(config));
+  app.listen(process.env.PORT || 10010);
 
-app.listen(process.env.PORT || 10010);
-
-console.log('try this:\nhttp://127.0.0.1:10010/weather?city=Kinston,NC');
+  console.log('try this:\nhttp://127.0.0.1:10010/weather?city=Kinston,NC');
+});
